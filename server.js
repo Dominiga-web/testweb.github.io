efca87092b84cd7590e011dd67676d0f780222db
Dominiga-web <594340668@qq.com>
2024-12-02 00:15:00 +0800

    update

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+// Enable CORS
+app.use(cors());
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'Example.html');
+  
+  // Read the file asynchronously
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(404).send('Not Found');
+      return;
+    }
+    res.send(data);
+  });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000');
+});
